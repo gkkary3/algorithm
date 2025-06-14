d7a1018948e71182a53900cf16a54e9c554cf51c
gkkary3 <nambawon1@naver.com>
2025-06-14 23:36:17 +0900

    3st_week Queue commit

---
+++ 3st_week/09_큐.py
@@ -0,0 +1,36 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Queue:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def enqueue(self, value):
+
+          new_node = Node(value)
+          if self.is_empty():
+              self.head = new_node
+              self.tail = new_node
+              return
+          self.tail.next = new_node
+          self.tail = new_node
+
+    def dequeue(self):
+        if self.is_empty():
+            return "Empty Queue"
+
+        del_head = self.head
+        self.head = self.head.next
+        return del_head.data
+
+    def peek(self):
+        if self.is_empty():
+            return "Empty Queue"
+        return self.head.data
+
+    def is_empty(self):
+        return self.head is None
