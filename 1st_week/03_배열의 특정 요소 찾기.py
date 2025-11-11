457388838cf8d12a39132abfe57cddc44dc7b451
daewonmediadev <daewonmediadev@daewonmediadevui-MacBookAir.local>
2025-11-11 13:37:24 +0900

    diffrent Method add

--- 1st_week/03_배열의 특정 요소 찾기.py
+++ 1st_week/03_배열의 특정 요소 찾기.py
@@ -1,10 +1,14 @@
 def is_number_exist(number, array):
 
-    for arr in array:
-        if arr == number:
-            return True
-    return False
+    # 방법1 O(n)
+    # for arr in array:
+    #     if arr == number:
+    #         return True
+    # return False
 
+    # 방법2. O(1) Set 사용 
+    array_set = set(array)
+    return number in array_set
 
 result = is_number_exist
 print("정답 = True 현재 풀이 값 =", result(3, [3,5,6,1,2,4]))
